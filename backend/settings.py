5d772eeb79a111fecc8339066953c102833b4e0c
mohammadSelimReza <srreza1999@gmail.com>
2024-10-04 22:38:29 +0600

    changed databased

--- backend/settings.py
+++ backend/settings.py
@@ -103,10 +103,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-     'default': dj_database_url.config(
-         default=env('POSTGRESQL'),
-     )
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
