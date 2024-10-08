664269062e3f47aead930fac8a2a899a8a4f1ddc
mohammadSelimReza <srreza1999@gmail.com>
2024-10-08 12:43:14 +0600

    database changed

--- app_core/views.py
+++ app_core/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
-from .models import BannerModel
-from .serializers import BannerSerializer
 from rest_framework.viewsets import  ModelViewSet
 from rest_framework.permissions import AllowAny
+from . import models
+from .serializers import BannerSerializer
 
 # Create your views here.
 class BannerView(ModelViewSet):
-    queryset = BannerModel.objects.all()
+    queryset = models.BannerModel.objects.all()
     serializer_class = BannerSerializer
     permission_classes = [AllowAny]
